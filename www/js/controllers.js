3a4e82d51c6958c9c478369f8bc7a764526c6383
Satbir Jhuti <satbirjhuti@gmail.com>
2015-11-29 00:09:54 -0500

    Add App Banners, styles

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('FeaturedCtrl', function($scope) {})
 
-.controller('ChartsCtrl', function($scope, Chats) {
+.controller('ChartsCtrl', function($scope, Charts) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,7 +11,7 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
+  $scope.chats = Charts.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
