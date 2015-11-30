bdfe803306d972b670be561adf3e26673a753f6e
Satbir Jhuti <satbirjhuti@gmail.com>
2015-11-29 22:24:29 -0500

    Featured tab now uses Parse SDK to get data

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,36 @@
-angular.module('starter.services', [])
+angular.module('iStore.services', [function() {
+  Parse.initialize("upAgOJr8FOxwKyxw9s4AqhcrhWkHvNLEuuhAGxjr", "l0S98W9uxxkkHfsxpPM8Yp3wvaWAbWhSqNhxiR7G");
+}])
+
+.factory('featuredService', ['$q', function($q) {
+  return {
+    getAppIcon: function(id) {
+      var defer = $q.defer(),
+        apps = Parse.Object.extend("apps"),
+        query = new Parse.Query(apps);
+
+      query.equalTo("objectId", id);
+      query.find().then(function(results) {
+        return defer.resolve(results);
+      });
+      return defer.promise;
+    },
+    getApps: function() {
+      var defer = $q.defer(),
+        apps = Parse.Object.extend("featured"),
+        query = new Parse.Query(apps);
+
+      query.find().then(function(results) {
+        return defer.resolve(results);
+      });
+      return defer.promise;
+    }
+  };
+}])
 
 .factory('Charts', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
+  /* Dummy data */
   var chats = [{
     id: 0,
     name: 'First App',
