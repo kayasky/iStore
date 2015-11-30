03fde05eaca10edb7c87dba53f66e9d4c107034d
Satbir Jhuti <satbirjhuti@gmail.com>
2015-11-29 22:20:58 -0500

    Minor Style fixes, app name changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,19 @@
-angular.module('starter.controllers', [])
+angular.module('iStore.controllers', [])
 
-.controller('FeaturedCtrl', function($scope) {})
+.controller('FeaturedCtrl', ['$scope', 'featuredService', function($scope, featuredService) {
+  var promise = featuredService.getApps();
+  promise.then(function(response) {
+    console.log(response);
+  }, function(response) {
+    console.log(response);
+  });
+}])
 
 .controller('ChartsCtrl', function($scope, Charts) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+
+  $scope.$on('$ionicView.enter', function(e) {
+    console.log('Entered Charts view');
+  });
 
   $scope.chats = Charts.all();
   $scope.remove = function(chat) {
